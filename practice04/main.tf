4a2d08120f71e3c305a262241c9b33c4e419d80a
javed1701 <25javed12@gmail.com>
2026-07-02 00:15:55 +0530

    remote backend v2

---
+++ practice04/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "name" {
+    ami = "ami-08f44e8eca9095668"
+    instance_type = "t3.micro"
+    key_name = "NEW-PRT-KEY"
+    tags = {
+        Name = "SBI"
+    }
+    availability_zone = "us-east-1b"
+    count = 1
+}
